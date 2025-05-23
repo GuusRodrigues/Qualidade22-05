7f861c81433e8cc09b571056107f7ce5902ab9ef
Thalysvl05 <thalysvl05@gmail.com>
2025-05-22 21:40:14 -0300

    att

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -18,24 +17,3 @@ def adicionar_usuario():
 
 if __name__ == '__main__':
     app.run(debug=True)
-=======
-from flask import Flask, request, jsonify
-
-app = Flask(__name__)
-usuarios = []
-
-@app.route('/usuarios', methods=['GET'])
-def listar_usuarios():
-    return jsonify(usuarios)
-
-@app.route('/usuarios', methods=['POST'])
-def adicionar_usuario():
-    data = request.get_json()
-    if not data.get('nome') or not data.get('email'):
-        return jsonify({'erro': 'Nome e email são obrigatórios!'}), 400
-    usuarios.append(data)
-    return jsonify(data), 201
-
-if __name__ == '__main__':
-    app.run(debug=True)
->>>>>>> d11eca09e5e3a608d896603715c943acc26c0d8f
